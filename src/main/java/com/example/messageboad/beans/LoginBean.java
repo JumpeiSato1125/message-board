0063b6fae4c9cc6a8b662a381ad78decae98fb72
maguro <tuna@maguronoMacBook-Pro.local>
2025-05-14 19:41:37 +0900

    ログインエラーチェックを修正、不要な画像を削除

--- src/main/java/com/example/messageboad/beans/LoginBean.java
+++ src/main/java/com/example/messageboad/beans/LoginBean.java
@@ -2,11 +2,15 @@ package com.example.messageboad.beans;
 
 import java.util.ArrayList;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class LoginBean {
+	@NotEmpty(message = "idを入力してください")
 	private String id;
+	@NotEmpty(message = "パスワードを入力してください")
 	private String password;
 	private ArrayList<String> errorList;
 	
--- src/main/java/com/example/messageboad/controller/LoginController.java
+++ src/main/java/com/example/messageboad/controller/LoginController.java
@@ -3,49 +3,57 @@ package com.example.messageboad.controller;
 import java.util.ArrayList;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.messageboad.beans.LoginBean;
 import com.example.messageboad.service.AccountService;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	AccountService accountService;
 
 	@GetMapping("/login")
-	public ModelAndView login(ModelAndView mav) {
-		mav.addObject("loginBean", new LoginBean());
-		mav.setViewName("login");
-		return mav;
+	public String login(Model model) {
+		if (!model.containsAttribute("loginBean")) {
+			LoginBean loginBean = new LoginBean();
+	        model.addAttribute("loginBean", loginBean);
+	    }
+		return "login";
 	}
 
 	@PostMapping("/login")
-	public ModelAndView form(@ModelAttribute LoginBean loginBean, HttpSession session, ModelAndView mav) {
-		
+	public ModelAndView form(@Valid @ModelAttribute LoginBean loginBean, BindingResult bindingResult,
+			HttpSession session, ModelAndView mav,RedirectAttributes redirectAttributes) {
+
+		// エラーチェック
+		if (bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("loginBean", loginBean);
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.loginBean",
+					bindingResult);
+			mav.setViewName("redirect:/login");
+			return mav;
+		}
+
 		// 変数宣言
 		String id = loginBean.getId();
 		String password = loginBean.getPassword();
-		
-		// エラーチェック
-		loginBean.setErrorList(errorCheck(id, password));
-		if(!loginBean.getErrorList().isEmpty()) {
-			// エラー画面に遷移
-			mav.addObject("loginBean", loginBean);
-			mav.setViewName("login");
-			return mav;
-		}
-		
+
+
 		// パスワードチェック
 		LoginBean lb = accountService.checkPassword(id, password);
-		if(lb == null) {
+		if (lb == null) {
 			// エラー画面に遷移
 			ArrayList<String> erorrList = new ArrayList<String>();
 			erorrList.add("パスワードが照合できませんでした。");
@@ -57,37 +65,15 @@ public class LoginController {
 			// セッションに登録して遷移
 			lb.setPassword(null);
 			session.setAttribute("lb", lb);
-			
+
 		}
 		mav.setViewName("redirect:/messageBoard/index");
 		return mav;
 	}
-	
+
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
-	    session.invalidate();
-	    return "redirect:/login";
-	}
-	
-	public ArrayList<String> errorCheck(String id, String password){
-		ArrayList<String> list = new ArrayList<String>();
-		if(id == null || id.isEmpty()) {
-			list.add("idを入力してください。");
-		}
-		if(!chekAlphaNumeral(id)) {
-			list.add("idは半角英数値を入力してください");
-		};
-		if(password == null || password.isEmpty()) {
-			list.add("passwordを入力してください。");
-		}
-		if(!chekAlphaNumeral(password)) {
-			list.add("パスワードは半角英数値を入力してください");
-		};
-		return list;
-	}
-	
-	public boolean chekAlphaNumeral(String s) {
-		if(s == null || s.isEmpty()) return false;
-		return s.matches("^[A-Za-z0-9]+$");
+		session.invalidate();
+		return "redirect:/login";
 	}
 }
